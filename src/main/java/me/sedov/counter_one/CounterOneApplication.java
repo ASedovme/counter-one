6cea942dafed5d6e5a344ce59d5aa32b4e3caca8
Aleksei <alexejsedow@gmail.com>
2024-10-17 20:20:30 +0300

    1

---
+++ src/main/java/me/sedov/counter_one/CounterOneApplication.java
@@ -0,0 +1,13 @@
+package me.sedov.counter_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sedov/counter_one/CounterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sedov.counter_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
